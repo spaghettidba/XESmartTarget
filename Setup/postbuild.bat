eb216971c7aed695fe7de14447de495f39bdad50
Gianluca Sartori <spaghettidba@sqlconsulting.it>
2020-07-17 16:21:05 +0200

    Removed local path to powershell

--- Setup/postbuild.bat
+++ Setup/postbuild.bat
@@ -1,2 +1,2 @@
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoProfile -NonInteractive -File %~dp0\SignMsi.ps1 -InputFile %1 -OutputFile %2
+powershell.exe -ExecutionPolicy Bypass -NoProfile -NonInteractive -File %~dp0\SignMsi.ps1 -InputFile %1 -OutputFile %2
 
