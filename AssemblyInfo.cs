76c7883a94e4b1037b3a52d92b4c933c9f7b5356
lalessioqd <luca.alessio@quantumdatis.com>
2025-03-13 11:51:38 +0100

    come nell'altro

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 //      Minor Version
 //      Build Number
 //      Revision
-//
+// 
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
