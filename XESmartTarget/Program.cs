1dd0154adcaf7cce89d5817afd02d23b6c162046
lalessioqd <luca.alessio@quantumdatis.com>
2025-04-08 09:05:29 +0200

    Update Program.cs

--- XESmartTarget/Program.cs
+++ XESmartTarget/Program.cs
@@ -16,29 +16,38 @@ namespace XESmartTarget
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Args count: " + args.Length);
-            foreach (var a in args)
-                Console.WriteLine("Arg: " + a);
-
             var result = Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(options => ProcessTarget(options));
         }
 
         private static void ProcessTarget(Options options)
         {
-            Console.WriteLine($"ProcessTarget invoked with: ConfigurationFile='{options.ConfigurationFile}', NoLogo={options.NoLogo}, Quiet={options.Quiet}, LogFile='{options.LogFile}', TimeoutSeconds={options.TimeoutSeconds}, PreExecutionScript='{options.PreExecutionScript}', PostExecutionScript='{options.PostExecutionScript}'");
-
+            string version = string.Empty;
             System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
-            string version = fvi.FileMajorPart.ToString() + "." + fvi.FileMinorPart.ToString() + "." + fvi.FileBuildPart.ToString();
+            if (!string.IsNullOrEmpty(assembly.Location))
+            {
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+                version = fvi.FileMajorPart.ToString() + "." + fvi.FileMinorPart.ToString() + "." + fvi.FileBuildPart.ToString();
+            }
+            else
+                version = "Linux";
 
+            // save the URI to a file and point configuration there
+            string tempPath = Path.GetTempPath();
+            if (string.IsNullOrEmpty(tempPath) && RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                tempPath = "/tmp/QMonitor";
+
+            if (!Directory.Exists(tempPath))
+                Directory.CreateDirectory(tempPath);
+
+            /*
             if (!options.NoLogo)
             {
                 Console.WriteLine(fvi.FileDescription + " " + version);
                 Console.WriteLine(fvi.LegalCopyright + " - " + fvi.CompanyName);
                 Console.WriteLine();
             }
-
+            */
 
             if (options.Quiet)
             {
@@ -88,14 +97,10 @@ namespace XESmartTarget
             // ******************************************
             Uri outUri;
             bool deleteTempFile = false;
-            Console.WriteLine($"Attempting to create a URI from ConfigurationFile='{options.ConfigurationFile}'");
-            bool isUri = Uri.TryCreate(options.ConfigurationFile, UriKind.Absolute, out outUri)
-               && (outUri.Scheme == Uri.UriSchemeHttp || outUri.Scheme == Uri.UriSchemeHttps);
-            Console.WriteLine($"Uri.TryCreate => {isUri}. outUri='{outUri}'");
+            bool isUri = Uri.TryCreate(options.ConfigurationFile, UriKind.Absolute, out outUri) && (outUri.Scheme == Uri.UriSchemeHttp || outUri.Scheme == Uri.UriSchemeHttps);
 
             if (isUri)
             {
-                Console.WriteLine($"Detected HTTP/HTTPS scheme: {outUri.Scheme}. UserInfo: {outUri.UserInfo}");
                 //password can be read from Windows Credentials
                 //if it exists, otherwise execution proceeds with user passed uri
                 try
@@ -106,8 +111,6 @@ namespace XESmartTarget
                     else
                         (username, password) = LinuxCredentialHelper.ReadCredential(outUri.OriginalString);
 
-                    Console.WriteLine($"Credentials retrieved? username: '{username}', password length: '{(password == null ? 0 : password.Length)}'");
-
                     if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
                     {
                         var uriBuilder = new UriBuilder(outUri)
@@ -117,7 +120,6 @@ namespace XESmartTarget
                         };
 
                         options.ConfigurationFile = uriBuilder.Uri.ToString();
-                        Console.WriteLine($"Rebuilt URI with credentials: {options.ConfigurationFile}");
                         Uri.TryCreate(options.ConfigurationFile, UriKind.Absolute, out outUri);
                     }
                 }
@@ -126,16 +128,9 @@ namespace XESmartTarget
                     logger.Error($"Couldn't retrieve Windows Credentials: {ex.Message}");
                 }
 
-                // save the URI to a file and point configuration there
-                string tempPath = Path.GetTempPath();
-                if (string.IsNullOrEmpty(tempPath) && RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                    tempPath = "/tmp/QMonitor";
-
-                if (!Directory.Exists(tempPath))
-                    Directory.CreateDirectory(tempPath);
+                
 
                 options.ConfigurationFile = Path.Combine(tempPath, $"{Guid.NewGuid()}.json");
-                Console.WriteLine($"Downloading configuration to temp file: {options.ConfigurationFile}");
 
                 using (var client = new HttpClient())
                 {
@@ -146,17 +141,14 @@ namespace XESmartTarget
                         {
                             var byteArray = Encoding.ASCII.GetBytes(outUri.UserInfo);
                             client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
-                            Console.WriteLine("Using Basic Auth from outUri.UserInfo");
                         }
                         var response = client.GetAsync(outUri).GetAwaiter().GetResult();
-                        Console.WriteLine($"HTTP response: {response.StatusCode} - {response.ReasonPhrase}");
                         if (response.IsSuccessStatusCode)
                         {
                             using (var fs = new FileStream(options.ConfigurationFile, FileMode.CreateNew))
                             {
                                 response.Content.CopyToAsync(fs).GetAwaiter().GetResult();
                             }
-                            Console.WriteLine($"File successfully downloaded to '{options.ConfigurationFile}'");
                         }
                         else
                         {
@@ -172,44 +164,35 @@ namespace XESmartTarget
                 deleteTempFile = true;
             }
 
-            Console.WriteLine($"Checking if file exists: '{options.ConfigurationFile}'");
             if (!File.Exists(options.ConfigurationFile))
             {
                 logger.Error(String.Format("File not found: '{0}'", options.ConfigurationFile));
-                Console.WriteLine("Run XESmartTarget -? for help.");
                 return;
             }
 
             // parse key value pairs
-            Console.WriteLine($"Parsing GlobalVariables (count: {options.GlobalVariables.Count()} )");
             foreach (var kvp in options.GlobalVariables)
             {
-                Console.WriteLine($"GlobalVariable: '{kvp}'");
                 var pair = kvp.Split('=');
                 TargetConfig.GlobalVariables.Add(pair[0], pair[1]);
             }
 
-            Console.WriteLine($"Loading TargetConfig from '{options.ConfigurationFile}'");
             TargetConfig config = TargetConfig.LoadFromFile(options.ConfigurationFile);
 
             // delete the file downloaded from URI
             if (deleteTempFile)
             {
-                Console.WriteLine($"deleteTempFile=true, checking if '{options.ConfigurationFile}' still exists before deleting");
                 if (File.Exists(options.ConfigurationFile))
                 {
                     File.Delete(options.ConfigurationFile);
-                    Console.WriteLine($"Temp file '{options.ConfigurationFile}' deleted.");
                 }
             }
 
             if (!String.IsNullOrEmpty(options.PreExecutionScript))
             {
-                Console.WriteLine($"PreExecutionScript is set: '{options.PreExecutionScript}'");
                 if (!File.Exists(options.PreExecutionScript))
                 {
                     logger.Error(String.Format("File not found: '{0}'", options.PreExecutionScript));
-                    Console.WriteLine("Run XESmartTarget -? for help.");
                     return;
                 }
                 else
@@ -224,11 +207,9 @@ namespace XESmartTarget
 
             if (!String.IsNullOrEmpty(options.PostExecutionScript))
             {
-                Console.WriteLine($"PostExecutionScript is set: '{options.PostExecutionScript}'");
                 if (!File.Exists(options.PostExecutionScript))
                 {
                     logger.Error(String.Format("File not found: '{0}'", options.PostExecutionScript));
-                    Console.WriteLine("Run XESmartTarget -? for help.");
                     return;
                 }
                 else
@@ -275,11 +256,9 @@ namespace XESmartTarget
             // delete the file downloaded from URI
             if (deleteTempFile)
             {
-                Console.WriteLine($"Final check to deleteTempFile: '{options.ConfigurationFile}'");
                 if (File.Exists(options.ConfigurationFile))
                 {
                     File.Delete(options.ConfigurationFile);
-                    Console.WriteLine($"Temp file '{options.ConfigurationFile}' deleted (final cleanup).");
                 }
             }
 
